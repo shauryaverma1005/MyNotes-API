69b3deeb0d2b506f5d0ae33db4cc0f648295ceaf
Shaurya <omshaurya941@gmail.com>
2025-12-05 21:06:19 +0530

    Packages List- jsonwebtoken, bcrypt, dotenv, express, mongoose, cors, nodemon, prettier

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -17,5 +18,18 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
-const User = mongoose.model("User", userSchema);
+// pre-middleware to encrypt password before saving
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){ return next()}
+
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
+
+// Method to match password entered
+userSchema.methods.matchPassword = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema);
 
